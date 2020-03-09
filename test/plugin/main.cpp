a54d4eda38f91aa2bc53e9bde9ca9c50ed2882c9
MagicYCC <376098946@qq.com>
2020-03-09 13:54:17 +0800

    add plugin_test project

---
+++ test/plugin/main.cpp
@@ -0,0 +1,8 @@
+#include "yhook_test.h"
+
+int main(int argc, char *argv[])
+{
+    YInlineHookTest();
+
+    return 0;
+}
---
+++ test/plugin/yhook_test.cpp
@@ -0,0 +1,65 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <plugin\yhook.h>
+#include "yhook_test.h"
+
+int Plus(int x, int y);
+
+// 从Plus + 3处开始hook
+extern "C" __declspec(naked) void YInlineHookPorc()
+{
+    printf("Enter YInlineHookPorc!\n");
+
+    _asm    ret
+}
+
+// 从Plus + 0x0C处开始hook
+extern "C" __declspec(naked) void YInlineHookPorc2()
+{
+    printf("Enter YInlineHookPorc2!\n");
+
+    _asm {
+        ret
+    }
+}
+
+// Plus = ::GetModuleHandle(NULL) + 0x000536E0
+int Plus(int x, int y)
+{
+    printf("Enter Plus!\n");
+	return x + y;
+}
+
+void YInlineHookTest()
+{
+    printf("Inline Hook Test!\n");
+
+    YInlineHook yInlineHook = YInlineHook();
+    yInlineHook.Set((BYTE *)::GetModuleHandle(NULL) + 0x000536E3/*Plus + 3*/,
+                    YInlineHookPorc, 6);
+
+    YInlineHook *yInlineHook2 = new YInlineHook();
+    yInlineHook2->Set((BYTE *)::GetModuleHandle(NULL) + 0x000536EC/*Plus + 0x0C*/,
+                      YInlineHookPorc2, 6);
+    Plus(1, 2);
+
+    // Hook Puls() + 3
+    yInlineHook.Load();
+    Plus(1, 2);
+
+    // Hook2 Puls() + 0x0C
+    yInlineHook2->Load();
+    Plus(1, 2);
+
+    // 卸载Hook
+    yInlineHook.Unload();
+    Plus(1, 2);
+
+    // 卸载Hook2
+    yInlineHook2->Unload();
+    Plus(1, 2);
+
+    delete yInlineHook2;
+
+    printf("Inline Hook Test End!\n");
+}
